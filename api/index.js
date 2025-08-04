d46c2b496df8006eddff3d23d1fca9013bfd3669
Ahmed NAgy <ahmednagy25t@gmail.com>
2025-08-04 15:51:45 +0300

    Create standalone API with all dependencies

--- api/index.js
+++ api/index.js
@@ -3,9 +3,9 @@ import dotenv from "dotenv";
 import cookieParser from "cookie-parser";
 import cors from "cors";
 
-import { connectDB } from "../server/src/lib/db.js";
-import authRoutes from "../server/src/routes/auth.route.js";
-import messageRoutes from "../server/src/routes/messages.route.js";
+import { connectDB } from "./lib/db.js";
+import authRoutes from "./routes/auth.route.js";
+import messageRoutes from "./routes/messages.route.js";
 
 dotenv.config();
 const app = express();
@@ -14,7 +14,11 @@ const app = express();
 const corsOptions = {
   origin:
     process.env.NODE_ENV === "production"
-      ? ["https://chatto-app.vercel.app", "https://chatto.vercel.app"]
+      ? [
+          "https://chatto-app.vercel.app",
+          "https://chatto.vercel.app",
+          "https://chatto-chatt-7hmtlfemv-ahmednagy25s-projects.vercel.app",
+        ]
       : ["http://localhost:5173"],
   credentials: true,
 };
