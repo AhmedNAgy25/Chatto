7367a9740a1c4f92ccacf10968d191cedff048c9
Ahmed NAgy <ahmednagy25t@gmail.com>
2025-08-04 16:03:01 +0300

    Add missing controllers and fix API routing

---
+++ api/controllers/message.controller.js
@@ -0,0 +1,74 @@
+import cloudinary from "../lib/cloudinary.js";
+import Message from "../models/message.model.js";
+import User from "../models/user.model.js";
+import mongoose from "mongoose";
+
+export const getUsersForSidebar = async (req, res) => {
+  try {
+    const loggedInUserId = req.user._id;
+    const filteredUsers = await User.find({
+      _id: { $ne: loggedInUserId },
+    }).select("-password");
+
+    res.status(200).json(filteredUsers);
+  } catch (error) {
+    console.log(`\nError in getUsersFromSidebar controller:`, error.message);
+    res.status(500).json({ message: "it's server error" });
+  }
+};
+
+export const getMessages = async (req, res) => {
+  try {
+    const { id: userToChatId } = req.params;
+    const myId = req.user._id;
+
+    // Validate that userToChatId is a valid ObjectId
+    if (!mongoose.Types.ObjectId.isValid(userToChatId)) {
+      return res.status(400).json({ message: "Invalid user ID" });
+    }
+
+    const messages = await Message.find({
+      $or: [
+        { senderId: myId, receiverId: userToChatId },
+        { senderId: userToChatId, receiverId: myId },
+      ],
+    });
+
+    res.status(200).json(messages);
+  } catch (error) {
+    console.log(`\nError in getMessages controller:`, error.message);
+    res.status(500).json({ message: "it's server error" });
+  }
+};
+
+export const sendMessage = async (req, res) => {
+  try {
+    const { text, image } = req.body;
+    const { id: receiverId } = req.params;
+    const senderId = req.user._id;
+
+    // Validate that receiverId is a valid ObjectId
+    if (!mongoose.Types.ObjectId.isValid(receiverId)) {
+      return res.status(400).json({ message: "Invalid receiver ID" });
+    }
+
+    let imageUrl;
+    if (image) {
+      const uploadRes = await cloudinary.uploader.upload(image);
+      imageUrl = uploadRes.secure_url;
+    }
+    const newMessage = new Message({
+      senderId,
+      receiverId,
+      text,
+      image: imageUrl,
+    });
+
+    await newMessage.save();
+
+    res.status(201).json(newMessage);
+  } catch (error) {
+    console.log(`\nError in sendMessages controller:`, error.message);
+    res.status(500).json({ message: "it's server error" });
+  }
+};
